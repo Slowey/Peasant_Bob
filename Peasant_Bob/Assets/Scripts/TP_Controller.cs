f6b7def4b7562635192cb8683bd7287aa6cf9721
slowey <asd>
2017-03-17 23:52:57 +0100

    started charactercontroller tutorial

---
+++ Peasant_Bob/Assets/Scripts/TP_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Controller : MonoBehaviour {
+
+    public static CharacterController m_characterController;
+    public static TP_Controller Instance;
+
+
+	// Use this for initialization
+	void Awake () {
+        m_characterController = GetComponent<CharacterController>();
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Camera.main == null)
+        {
+            return;
+        }
+        GetLocomotionInput();
+        TP_Motor.m_instance.UpdateMotor();
+	}
+    void GetLocomotionInput()
+    {
+        float deadZone = 0.1f;
+        TP_Motor.m_instance.m_moveVector = Vector3.zero;
+        float t_vert = Input.GetAxis("Vertical");
+        float t_hori = Input.GetAxis("Horizontal");
+        if (t_vert  > deadZone || t_vert < -deadZone)
+        {
+            TP_Motor.m_instance.m_moveVector += new Vector3(0,0, t_vert);
+        }
+        if (t_hori > deadZone || t_hori < -deadZone)
+        {
+            TP_Motor.m_instance.m_moveVector += new Vector3(t_hori, 0, 0);
+        }
+
+    }
+}
---
+++ Peasant_Bob/Assets/Scripts/TP_Motor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Motor : MonoBehaviour {
+
+    public static TP_Motor m_instance;
+    public float m_moveSpeed = 10f;
+    public Vector3 m_moveVector { get; set; }
+	// Use this for initialization
+	void Awake () {
+        m_instance = this;
+	}
+	
+	// Update is called once per frame
+	public void UpdateMotor () {
+        SnapAlignCharacterWithCamera();
+        ProcessMotion();
+	}
+
+    void ProcessMotion()
+    {
+        // Transform MoveVector to Wolrd Space
+        m_moveVector = transform.TransformDirection(m_moveVector);
+
+        // Normalize movevec if mag > 1
+        if (m_moveVector.magnitude > 1)
+        {
+            m_moveVector = Vector3.Normalize(m_moveVector);
+        }
+        // multiply normalised movevec with movespeed
+        m_moveVector *= m_moveSpeed;
+        // Multiply MoveVector by DeltaTime.
+        m_moveVector *= Time.deltaTime;
+        // Move the Character in World space
+        TP_Controller.m_characterController.Move(m_moveVector);
+    }
+
+    void SnapAlignCharacterWithCamera()
+    {
+        if (m_moveVector.x != 0 || m_moveVector.z!=0)
+        {
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x,
+                Camera.main.transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+    }
+}
