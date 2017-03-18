f9ecf18923387b77c9f390929f99a299a5d848c8
Akerspoket <konrad_o_92@hotmail.com>
2017-03-18 23:50:25 +0100

    start of placing units

--- Peasant_Bob/Assets/Scripts/PlayerScripts/InputCheckScript.cs
+++ Peasant_Bob/Assets/Scripts/PlayerScripts/InputCheckScript.cs
@@ -10,19 +10,19 @@ public class InputCheckScript : MonoBehaviour {
     // Run all input form here
     StructurePlacementSystem placeMentSystem;
     BuildableStructureSystem buildSelectSystem;
-
+    UnitPlacementSystem unitPlacementSystem;
 
     StructureMenu activeMenu;
 	// Use this for initialization
 	void Start () {
         placeMentSystem = GetComponent<StructurePlacementSystem>();
         buildSelectSystem = GetComponent<BuildableStructureSystem>();
-		
+        unitPlacementSystem = GetComponent<UnitPlacementSystem>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        unitPlacementSystem.UpdateSystem();
         if(buildSelectSystem.UpdateSystem())
         {
             // cancel others
@@ -38,9 +38,20 @@ public class InputCheckScript : MonoBehaviour {
         }
         if (Input.GetKeyDown(KeyCode.E) && activeMenu == null)
         {
-            CheckRayCastHit(); 
+            CheckRayCastHit("Buildings"); 
             placeMentSystem.CancelPlace();
             
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (unitPlacementSystem.UnitToPlace == null)
+            {
+                CheckRayCastHit("Units");              
+            }
+            else if (unitPlacementSystem.UnitToPlace != null)
+            {
+                unitPlacementSystem.PlaceUnit();
+            }
         }
         if(Input.GetKeyDown(KeyCode.Escape))
         {
@@ -51,11 +62,12 @@ public class InputCheckScript : MonoBehaviour {
                 activeMenu = null;
                 placeMentSystem.CancelPlace();
             }
+            unitPlacementSystem.UnitToPlace = null;
         }
 
 	}
 
-    void CheckRayCastHit()
+    void CheckRayCastHit(string layer)
     {
         // Cast ray
         Vector3 direction;
@@ -73,13 +85,18 @@ public class InputCheckScript : MonoBehaviour {
         newRay.direction = direction;
         RaycastHit hitInfo;
 
-        if(Physics.Raycast(newRay, out hitInfo, 50, LayerMask.GetMask("Buildings", "Units")))
+        if(Physics.Raycast(newRay, out hitInfo, 50, LayerMask.GetMask(layer)))
         {
             StructureMenu menu = hitInfo.transform.GetComponent<StructureMenu>();
             if (menu != null)
             {
                 activeMenu = menu;
                 activeMenu.CreateMenu();
+            }
+            NavAgent navigation = hitInfo.transform.GetComponent<NavAgent>();
+            if (navigation != null && navigation.m_type != NavAgent.UnitType.Peasant)
+            {
+                unitPlacementSystem.UnitToPlace = navigation.gameObject;
             }
             // Else if on unti menu
         }
---
+++ Peasant_Bob/Assets/Scripts/UnitScripts/UnitPlacementSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPlacementSystem : MonoBehaviour {
+    public GameObject UnitToPlace = null;
+    public Camera camera;
+    public GameObject pointer;
+    private Vector3 placePosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public void UpdateSystem()
+    {
+        if (UnitToPlace == null)
+        {
+            return;
+        }
+        pointer.GetComponent<MeshRenderer>().enabled = true;
+        Vector3 direction;
+        if (camera == null)
+        {
+            direction = transform.forward; // Get from camera
+        }
+        else
+        {
+            direction = camera.transform.forward;
+        }
+
+        Ray ray = new Ray(transform.position + direction * 0.2f, direction);
+        //Ray ray = new Ray(new Vector3(0, 5, 1), Vector3.down);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 1000.0f))
+        {
+            placePosition = hitInfo.point;
+            pointer.transform.position = placePosition;
+        }
+    }
+
+    public void PlaceUnit()
+    {
+        if (UnitToPlace != null)
+        {
+            UnitToPlace.GetComponent<NavAgent>().SetDestination(placePosition, AgentsManager.AgentStates.Fighting);
+        }
+        UnitToPlace = null;
+        pointer.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
