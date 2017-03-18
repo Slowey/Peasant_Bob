c3920ce0116192cc89db2724feff0c59207de070
Jawsarn <christian_markowicz@hotmail.com>
2017-03-18 19:23:20 +0100

    Added bed system

---
+++ Peasant_Bob/Assets/Scripts/BedSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedSystem : MonoBehaviour {
+
+    public static BedSystem bedSystem;
+
+    int occopiedBeds = 0;
+    int maxBeds = 0;
+
+    void Awake()
+    {
+        if (bedSystem != null)
+        {
+            Debug.LogError("Multiple bed systems!");
+        }
+
+        bedSystem = this;
+    }
+
+    int GetNumFreeBeds()
+    {
+        return maxBeds - occopiedBeds;
+    }
+
+    public void OccopyBed(int number)
+    {
+        occopiedBeds += number;
+    }
+
+    public void FreeBeds(int number)
+    {
+        occopiedBeds -= number;
+    }
+
+    public void AddBeds(int number)
+    {
+        maxBeds += number;
+    }
+    public void RemoveBeds(int number)
+    {
+        maxBeds -= number;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Peasant_Bob/Assets/Scripts/GenerateBeds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBeds : MonoBehaviour {
+    public int bedAmount;
+
+	// Use this for initialization
+	void Start () {
+        BedSystem.bedSystem.AddBeds(bedAmount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDestroy()
+    {
+        BedSystem.bedSystem.RemoveBeds(bedAmount);
+    }
+}
