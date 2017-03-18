2cd7bc61f1b0ae5ddccf10b4af62e8b470b66248
Jawsarn <christian_markowicz@hotmail.com>
2017-03-18 21:51:24 +0100

    Added team scripts

---
+++ Peasant_Bob/Assets/Scripts/Team.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour {
+
+    public enum Teams
+    {
+        Blue,
+        Red,
+        Yellow,
+    }
+
+    public Teams team;
+}
