38465e164f9c8b1a5e350178dbb5d8b04d4e84a6
Jawsarn <christian_markowicz@hotmail.com>
2017-03-19 18:51:55 +0100

    Fixing prefabs

---
+++ Peasant_Bob/Assets/Scripts/SetColorOnStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColorOnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SetTeamColor.SetColorForObject(gameObject, GetComponent<Team>().team);
+	}
+}
