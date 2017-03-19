799421950992f0a6aef606b0879be52de973877e
Jawsarn <christian_markowicz@hotmail.com>
2017-03-19 12:13:04 +0100

    Added free resource changed loc of prefabs

---
+++ Peasant_Bob/Assets/Scripts/FreeResourcesOnDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeResourcesOnDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDestroy()
+    {
+        UnitInformation unitInfo = GetComponent<UnitInformation>();
+        if (unitInfo)
+        {
+            BedSystem.bedSystem.FreeBeds(unitInfo.requireBeds);
+        }
+    }
+}
