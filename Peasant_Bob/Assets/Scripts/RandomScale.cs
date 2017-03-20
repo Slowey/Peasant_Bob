0a485a00d88c1c50393e2f2c9f6301132037e9f4
Christian Markowicz <Christian Markowicz>
2017-03-20 23:40:03 +0100

    Scaling stuff

---
+++ Peasant_Bob/Assets/Scripts/RandomScale.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float scale = Random.Range(0.75f, 3.0f);
+        transform.localScale = new Vector3(scale, scale, scale);
+	}
+	
+}
